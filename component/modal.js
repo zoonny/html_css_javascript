794c76ef9760710db0c72bd0125f0d079798c559
hyung <hyung@kt.com>
2022-04-04 17:59:07 +0900

    modal

---
+++ component/modal.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector(".modal");
+const openBtn = document.querySelector("button[class=btn_open]");
+const closeBtn = document.querySelector("button[class=btn_close]");
+const modalBackground = modal.querySelector(".modal_background");
+
+function displayModal() {
+    modal.classList.toggle("hidden");
+}
+
+openBtn.addEventListener("click", displayModal);
+closeBtn.addEventListener("click", displayModal);
+modalBackground.addEventListener("click", displayModal);
